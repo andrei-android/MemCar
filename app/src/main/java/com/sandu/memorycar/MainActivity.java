00453ebeb12ab0abc3fae61ee1aabfd72c8204e9
Andrei Sandu <sandu.andrei86@gmail.com>
2020-11-25 23:17:54 +0000

    Created layout for the home screen

--- app/src/main/java/com/sandu/memorycar/MainActivity.java
+++ app/src/main/java/com/sandu/memorycar/MainActivity.java
@@ -3,11 +3,15 @@ package com.sandu.memorycar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_mainScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+
+        btn_mainScreen = findViewById(R.id.btn_mainScreen);
+
+        btn_mainScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+        });
     }
 }
